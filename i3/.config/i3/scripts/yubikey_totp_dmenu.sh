3e1e0124ac99a88c9de7d2f5495533deff96ea50
laburnumT <laburnumT@users.noreply.github.com>
2023-10-11 20:25:33 +0200

    Clean up code

--- i3/.config/i3/scripts/yubikey_totp_dmenu.sh
+++ i3/.config/i3/scripts/yubikey_totp_dmenu.sh
@@ -2,15 +2,13 @@
 
 yk_cnt=$(ykman list | wc -l)
 
-if [[ $yk_cnt -eq 0 ]]; then
+if [[ $yk_cnt -eq 1 ]]; then
+        account=$(ykman oath accounts list | dmenu -i)
+        if [[ -n $account ]]; then
+                ykman oath accounts code -s "${account}" | xclip -rmlastnl -selection clipboard
+        fi
+elif [[ $yk_cnt -eq 0 ]]; then
         i3-nagbar --message "No yubikey inserted"
-        exit 1
 elif [[ $yk_cnt -gt 1 ]]; then
         i3-nagbar --message "More than one yubikey inserted"
-        exit 1
-fi
-
-account=$(ykman oath accounts list | dmenu -i)
-if [[ -n $account ]]; then
-        ykman oath accounts code -s "${account}" | xclip -rmlastnl -selection clipboard
 fi
